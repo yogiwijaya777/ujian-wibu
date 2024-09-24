013204617c5a05418bed6d104e5becefd77c515d
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-09-25 03:34:38 +0700

    Deploy to vercel 4

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vercel from "@vitejs/plugin-vercel";
+import vercel from "vite-plugin-vercel";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
